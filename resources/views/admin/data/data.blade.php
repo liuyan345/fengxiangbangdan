36bfffa5f5cd2c4e132ef20f367edef9f29ad3d0
liuyan <1260192349@qq.com>
2019-05-18 11:29:39 +0800

    1

--- resources/views/admin/data/data.blade.php
+++ resources/views/admin/data/data.blade.php
@@ -183,102 +183,102 @@
 @section('page_javascript')
     <script>
 
-        {{--var EcommerceOrders = function () {--}}
+        var EcommerceOrders = function () {
 
-            {{--var handleOrders = function () {--}}
-                {{--var grid = new Datatable();--}}
-                {{--grid.init({--}}
-                    {{--src: $("#datatable_orders"),--}}
-                    {{--onSuccess: function (grid) {--}}
-                    {{--},--}}
-                    {{--onError: function (grid) {--}}
-                        {{--// execute some code on network or other general error--}}
-                    {{--},--}}
-                    {{--loadingMessage: 'Loading...',--}}
-                    {{--dataTable: {--}}
-                        {{--"dom": "<'row'<'col-md-12 col-sm-12'<'table-group-actions pull-left'>>r>t<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'>>",--}}
-                        {{--"lengthMenu": [--}}
-                            {{--[10, 20, 50, 100, 150],--}}
-                            {{--[10, 20, 50, 100, 150] // change per page values here--}}
-                        {{--],--}}
-                        {{--"sAjaxDataProp" : "data",--}}
-                        {{--"pageLength": 10, // default record count per page--}}
-                        {{--"ajax": {--}}
-                            {{--"url": "/admin/data/datalist", // ajax source--}}
-                            {{--"type": "POST",--}}
-                            {{--"data": function (d) {--}}
-                                {{--d._token = "{{csrf_token()}}";--}}
-                                {{--d.channel_name = $("#channel_name").val();--}}
-                                {{--d.ad_name = $("#ad_name").val();--}}
-                                {{--d.pack_name = $("#pack_name").val();--}}
-                                {{--d.start_time = $("#start_time").val();--}}
-                                {{--d.end_time = $("#end_time").val();--}}
-                            {{--}--}}
-                        {{--},--}}
-                        {{--'sort': false,--}}
-                        {{--"sPaginationType": "full_numbers",      //翻页界面类型--}}
-                        {{--"oLanguage": {                          //汉化--}}
-                            {{--"sLengthMenu": "每页显示 _MENU_ 条记录",--}}
-                            {{--"sZeroRecords": "没有检索到数据",--}}
-                            {{--"sInfo": "当前数据为从第 _START_ 到第 _END_ 条数据；总共有 _TOTAL_ 条记录",--}}
-                            {{--"sInfoEmtpy": "没有数据",--}}
-                            {{--"sProcessing": "正在加载数据...",--}}
-                            {{--"oPaginate": {--}}
-                                {{--"sFirst": "首页",--}}
-                                {{--"sPrevious": "前页",--}}
-                                {{--"sNext": "后页",--}}
-                                {{--"sLast": "尾页"--}}
-                            {{--}--}}
-                        {{--},--}}
-                        {{--'aoColumns':[--}}
-                            {{--{'mData':'id'},--}}
-                            {{--{'mData':'cdate'},--}}
-                            {{--{'mData':'channel_name'},--}}
-                            {{--{'mData':'ad_name'},--}}
-                            {{--{'mData':'pack_name'},--}}
-                            {{--{'mData':'type'},--}}
-{{--//                            {'mData':function(lineData){--}}
-{{--//                                if(lineData.status == 1){--}}
-{{--//                                    return "<span style='color:green'>上架</span>";--}}
-{{--//                                }else if(lineData.status == 2){--}}
-{{--//                                    return "<span style='color:red'>下架</span>";--}}
-{{--//                                }--}}
-{{--//                            }},--}}
-                            {{--{'mData':'price'},--}}
-                            {{--{'mData':'data'},--}}
-                            {{--{'mData':'money'},--}}
-                        {{--],--}}
+            var handleOrders = function () {
+                var grid = new Datatable();
+                grid.init({
+                    src: $("#datatable_orders"),
+                    onSuccess: function (grid) {
+                    },
+                    onError: function (grid) {
+                        // execute some code on network or other general error
+                    },
+                    loadingMessage: 'Loading...',
+                    dataTable: {
+                        "dom": "<'row'<'col-md-12 col-sm-12'<'table-group-actions pull-left'>>r>t<'row'<'col-md-8 col-sm-12'pli><'col-md-4 col-sm-12'>>",
+                        "lengthMenu": [
+                            [10, 20, 50, 100, 150],
+                            [10, 20, 50, 100, 150] // change per page values here
+                        ],
+                        "sAjaxDataProp" : "data",
+                        "pageLength": 10, // default record count per page
+                        "ajax": {
+                            "url": "/admin/data/datalist", // ajax source
+                            "type": "POST",
+                            "data": function (d) {
+                                d._token = "{{csrf_token()}}";
+                                d.channel_name = $("#channel_name").val();
+                                d.ad_name = $("#ad_name").val();
+                                d.pack_name = $("#pack_name").val();
+                                d.start_time = $("#start_time").val();
+                                d.end_time = $("#end_time").val();
+                            }
+                        },
+                        'sort': false,
+                        "sPaginationType": "full_numbers",      //翻页界面类型
+                        "oLanguage": {                          //汉化
+                            "sLengthMenu": "每页显示 _MENU_ 条记录",
+                            "sZeroRecords": "没有检索到数据",
+                            "sInfo": "当前数据为从第 _START_ 到第 _END_ 条数据；总共有 _TOTAL_ 条记录",
+                            "sInfoEmtpy": "没有数据",
+                            "sProcessing": "正在加载数据...",
+                            "oPaginate": {
+                                "sFirst": "首页",
+                                "sPrevious": "前页",
+                                "sNext": "后页",
+                                "sLast": "尾页"
+                            }
+                        },
+                        'aoColumns':[
+                            {'mData':'id'},
+                            {'mData':'cdate'},
+                            {'mData':'channel_name'},
+                            {'mData':'ad_name'},
+                            {'mData':'pack_name'},
+                            {'mData':'type'},
+//                            {'mData':function(lineData){
+//                                if(lineData.status == 1){
+//                                    return "<span style='color:green'>上架</span>";
+//                                }else if(lineData.status == 2){
+//                                    return "<span style='color:red'>下架</span>";
+//                                }
+//                            }},
+                            {'mData':'price'},
+                            {'mData':'data'},
+                            {'mData':'money'},
+                        ],
 
-                        {{--"aoColumnDefs": [--}}
-                            {{--{--}}
-                                {{--'orderable': false,--}}
-                                {{--'targets': '_all'--}}
-                            {{--}--}}
-                        {{--],--}}
-                    {{--}--}}
-                {{--});--}}
-                {{--grid.getTableWrapper().on('click', '.table-group-action-submit', function (e) {--}}
-                    {{--//自己定义的搜索框，可以是时间控件，或者checkbox 等等--}}
-                    {{--grid.getDataTable().ajax.reload();--}}
-                {{--});--}}
+                        "aoColumnDefs": [
+                            {
+                                'orderable': false,
+                                'targets': '_all'
+                            }
+                        ],
+                    }
+                });
+                grid.getTableWrapper().on('click', '.table-group-action-submit', function (e) {
+                    //自己定义的搜索框，可以是时间控件，或者checkbox 等等
+                    grid.getDataTable().ajax.reload();
+                });
 
-            {{--}--}}
+            }
 
-            {{--return {--}}
+            return {
 
-                {{--init: function () {--}}
-                    {{--handleOrders();--}}
-                {{--}--}}
+                init: function () {
+                    handleOrders();
+                }
 
-            {{--};--}}
+            };
 
-        {{--}();--}}
+        }();
 
-        {{--if (App.isAngularJsApp() === false) {--}}
-            {{--jQuery(document).ready(function() {--}}
-                {{--EcommerceOrders.init();--}}
-            {{--});--}}
-        {{--}--}}
+        if (App.isAngularJsApp() === false) {
+            jQuery(document).ready(function() {
+                EcommerceOrders.init();
+            });
+        }
 
         var toastr = {};
         toastr.options = {
