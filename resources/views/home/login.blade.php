defee423e0e6e1371cc64d86f18a59523c47c180
liuyan <1260192349@qq.com>
2019-05-20 18:45:40 +0800

    t

--- resources/views/home/login.blade.php
+++ resources/views/home/login.blade.php
@@ -67,25 +67,49 @@
                 <a href="javascript:;" id="register-btn" class="uppercase">注册新渠道商</a>
             </p>
         </div>
-        {{--<div class="login-options">--}}
-            {{--<h4>Or login with</h4>--}}
-            {{--<ul class="social-icons">--}}
-                {{--<li>--}}
-                    {{--<a class="social-icon-color facebook" data-original-title="facebook" href="javascript:;"></a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="social-icon-color twitter" data-original-title="Twitter" href="javascript:;"></a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="social-icon-color googleplus" data-original-title="Goole Plus" href="javascript:;"></a>--}}
-                {{--</li>--}}
-                {{--<li>--}}
-                    {{--<a class="social-icon-color linkedin" data-original-title="Linkedin" href="javascript:;"></a>--}}
-                {{--</li>--}}
-            {{--</ul>--}}
-        {{--</div>--}}
-
     </form>
+    <!-- BEGIN REGISTRATION FORM -->
+    <form class="register-form" action="index.html" method="post">
+        <h3 class="font-green">Sign Up</h3>
+        <p class="hint"> Enter your personal details below: </p>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Full Name</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Full Name" name="fullname" /> </div>
+        <div class="form-group">
+            <!--ie8, ie9 does not support html5 placeholder, so we just show field title for that-->
+            <label class="control-label visible-ie8 visible-ie9">Email</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Email" name="email" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Address</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="Address" name="address" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">City/Town</label>
+            <input class="form-control placeholder-no-fix" type="text" placeholder="City/Town" name="city" /> </div>
+        <p class="hint"> Enter your account details below: </p>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Username</label>
+            <input class="form-control placeholder-no-fix" type="text" autocomplete="off" placeholder="Username" name="username" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Password</label>
+            <input class="form-control placeholder-no-fix" type="password" autocomplete="off" id="register_password" placeholder="Password" name="password" /> </div>
+        <div class="form-group">
+            <label class="control-label visible-ie8 visible-ie9">Re-type Your Password</label>
+            <input class="form-control placeholder-no-fix" type="password" autocomplete="off" placeholder="Re-type Your Password" name="rpassword" /> </div>
+        <div class="form-group margin-top-20 margin-bottom-20">
+            <label class="mt-checkbox mt-checkbox-outline">
+                <input type="checkbox" name="tnc" /> I agree to the
+                <a href="javascript:;">Terms of Service </a> &
+                <a href="javascript:;">Privacy Policy </a>
+                <span></span>
+            </label>
+            <div id="register_tnc_error"> </div>
+        </div>
+        <div class="form-actions">
+            <button type="button" id="register-back-btn" class="btn green btn-outline">Back</button>
+            <button type="submit" id="register-submit-btn" class="btn btn-success uppercase pull-right">Submit</button>
+        </div>
+    </form>
+    <!-- END REGISTRATION FORM -->
 </div>
 <!--[if lt IE 9]>
 <script src="{{asset('/style_js/cpcAdmin/assets/global/plugins/respond.min.js')}}"></script>
